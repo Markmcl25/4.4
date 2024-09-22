1d0c46ab947f48d88753dca9ce50e73b0983ffdf
Mark Mcloughlin <mark25mky@gmail.com>
2024-09-22 17:05:30 +0000

    added new path in in urls.py to show success page

--- readitnews/readitnews/urls.py
+++ readitnews/readitnews/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('sports/', sports_view, name='sports'),
     path('technology/', technology_view, name='technology'),
     path('create/', create_article_view, name='create_article'),
+    path('success/', lambda request: render(request, 'success.html'), name='success'),  # Simple success page
 ]
