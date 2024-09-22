a1794cd2b2335ad24c8421e2e717b5bc8841ae92
Mark Mcloughlin <mark25mky@gmail.com>
2024-09-22 19:29:19 +0000

    installed and added gunicorn to requirments file

--- readitnews/readitnews/views.py
+++ readitnews/readitnews/views.py
@@ -9,7 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('/workspace/4.4/readitnews/creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('read-it-news')
